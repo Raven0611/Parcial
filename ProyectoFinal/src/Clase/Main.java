99ce6bce40abc7ab3aafac7687b69e9a4241c40e
Dialejo24 <dialejosi530@gmail.com>
2024-05-06 17:27:20 -0500

    Raven status

--- ProyectoFinal/src/Clase/Main.java
+++ ProyectoFinal/src/Clase/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         int[][] x = {{2, 3, 2, 5}, {3, 3, 2, 5}, {1, 4, 3, 2}, {2, 3, 4, 5}};
-
+        //raven es una perra sarnosa
         Clinica clinica = new Clinica("Ayshat", x);
 
         System.out.println(clinica.toString());
