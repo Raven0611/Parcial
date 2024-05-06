7368411bd5a009169ff30e0c07a53ebeb7d0aff5
Raven0611 <jhobitiflo2000@gmail.com>
2024-05-06 11:33:40 -0500

    Primera Actualizacion

---
+++ ProyectoFinal/src/Clase/Camilla.java
@@ -0,0 +1,15 @@
+package Clase;
+
+public class Camilla {
+
+    private int id;
+    private boolean disponible;
+
+    public Camilla(int id) {
+        this.id = id;
+        this.disponible=true;
+    }
+    
+    
+    
+}
